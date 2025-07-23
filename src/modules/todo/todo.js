111ce1308a1928b3beb00d6ffd79bc6f3303c1eb
Jack Harvey <53987140+Jack-harvey@users.noreply.github.com>
2025-07-23 18:14:14 +1000

    Update what a todo is

--- src/modules/todo/todo.js
+++ src/modules/todo/todo.js
@@ -1,11 +1,13 @@
 import { format } from "date-fns";
 export class Todo {
-  constructor(title, description, dueDate, projectId) {
+  constructor(title, description, dueDate, projectId, isUrgent) {
     this.id = crypto.randomUUID();
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
     this.projectId = projectId;
     this.createdDate = format(new Date(), "dd/MM/yyyy");
+    this.completedDate = null;
+    this.isUrgent = isUrgent;
   }
 }
