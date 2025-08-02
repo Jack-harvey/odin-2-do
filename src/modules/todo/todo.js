a4ecf01fa28b11a24eb20fbe313f245d43056e7e
Jack Harvey <53987140+Jack-harvey@users.noreply.github.com>
2025-08-03 09:40:43 +1000

    Change the layout

--- src/modules/todo/todo.js
+++ src/modules/todo/todo.js
@@ -11,3 +11,25 @@ export class Todo {
     this.isUrgent = isUrgent;
   }
 }
+
+//create table, create a function for a single row, create a loop for each todo, add table to page
+
+const createTableElement = function () {
+  const tableEl = document.createElement("table");
+  tableEl.classList.add("todo-table");
+  tableEl.innerHTML = `
+          <tr class="table-header">
+            <th></th>
+            <th>Todo</th>
+            <th>Notes</th>
+            <th>Due-Date</th>
+          </tr>
+  `;
+
+  return tableEl;
+};
+
+const createTableRow = function (todo) {
+  const tableRow = document.createElement("tr");
+  tableRow.dataset.id = todo.id;
+};
