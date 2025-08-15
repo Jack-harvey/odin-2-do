8717776bb646e8bb60e6f358a58f5ccaf6aa72ee
Jack Harvey <53987140+Jack-harvey@users.noreply.github.com>
2025-08-15 18:08:39 +1000

    Add function to display a list of projects in sidebar

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import {
   getAllTodosWithinDayTimeFrame,
 } from "./localStorage/localStorage";
 import { attachTodoTable } from "./modules/todo/todo";
+import { renderAllProjectsToSideBar } from "./modules/project/project";
 
 checkIfLocalStorageDataExists();
 
 const TESTINGID = "481447ca-d37e-4546-94c9-0953b6831b8d";
 
+renderAllProjectsToSideBar();
 attachTodoTable(TESTINGID);
 
 console.log(getAllTodosWithinDayTimeFrame(3));
--- src/modules/project/project.js
+++ src/modules/project/project.js
@@ -1,4 +1,5 @@
-import { format } from "date-fns";
+import { compareAsc, format } from "date-fns";
+import { getAllProjects } from "../../localStorage/localStorage";
 export class Project {
   constructor(title, description) {
     this.id = crypto.randomUUID();
@@ -7,3 +8,21 @@ export class Project {
     this.createdDate = format(new Date(), "dd/MM/yyyy");
   }
 }
+
+export const renderAllProjectsToSideBar = function () {
+  // get a list of projects, for each name, format it into the bar
+  const allProjects = getAllProjects();
+  const allProjectsSorted = allProjects.sort(compareAsc);
+  const projectListElement = document.querySelector(".project-list");
+
+  allProjectsSorted.forEach((Project) => {
+    const projectId = Project.id;
+    const projectName = Project.title;
+
+    const listItemElement = document.createElement("li");
+    listItemElement.textContent = projectName;
+    listItemElement.dataset.id = projectId;
+
+    projectListElement.append(listItemElement);
+  });
+};
